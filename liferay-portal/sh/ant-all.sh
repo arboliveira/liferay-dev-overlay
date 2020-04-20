4bf2a056d97951b04c2471851044cf10fe18379f
André de Oliveira <andre.oliveira@liferay.com>
2020-04-19 23:48:17 -0700

    ant setup-profile-dxp

--- liferay-portal/sh/ant-all.sh
+++ liferay-portal/sh/ant-all.sh
@@ -2,7 +2,34 @@
 
 set -o errexit ; set -o nounset
 
+
+#
+function modify_build_xml()
+{
 cd ${LIFERAY_PORTAL_DIR}
+
+cp build.xml build.original.xml
+
+cat build.original.xml | \
+sed -e 's/git clean -df/# git clean -df/g' > build.xml
+
+rm build.original.xml
+
+cd -
+}
+#
+
+
+################
+
+
+# modify_build_xml
+
+cd ${LIFERAY_PORTAL_DIR}
+
+figlet ant setup-profile-dxp || true
+
+ant setup-profile-dxp
 
 figlet Keep calm and ANT ALL || true
 
@@ -18,3 +45,5 @@ fi
 figlet compile-test || true
 
 ant compile-test
+
+mkdir ${LIFERAY_BUNDLES_PORTAL_DIR}/osgi/log4j
