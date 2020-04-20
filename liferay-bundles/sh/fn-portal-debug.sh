a0dc3b27ce59884356d16f874e971b19fde1181c
André de Oliveira <andre.oliveira@liferay.com>
2020-04-19 23:42:52 -0700

    serverTimezone

--- liferay-bundles/sh/fn-portal-debug.sh
+++ liferay-bundles/sh/fn-portal-debug.sh
@@ -12,8 +12,9 @@ function tomcat_debug()
 	LPORTAL_DATABASE=$1
 	TOMCAT_DIR=$2
 
-	property_set    jdbc.default.url     jdbc:mysql://localhost/${LPORTAL_DATABASE}?useUnicode=true\\\&characterEncoding=UTF-8\\\&useFastDateParsing=false     ${TOMCAT_DIR}/../portal-ext.properties
+	property_set    jdbc.default.url     jdbc:mysql://localhost/${LPORTAL_DATABASE}?useUnicode=true\\\&characterEncoding=UTF-8\\\&useFastDateParsing=false\\\&serverTimezone=GMT     ${TOMCAT_DIR}/../portal-ext.properties
 	# ${TOMCAT_DIR}/webapps/ROOT/WEB-INF/classes/portal-ext.properties
 
-	${TOMCAT_DIR}/bin/catalina.sh jpda run
+	cd ${TOMCAT_DIR}
+	bin/catalina.sh jpda run
 }
