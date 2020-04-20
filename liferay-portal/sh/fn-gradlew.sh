6d12ee5169b8e9b8bbc3ba0cd32939cea0b470e2
André de Oliveira <andre.oliveira@liferay.com>
2020-04-19 23:51:50 -0700

    gradlew deploy quietly

--- liferay-portal/sh/fn-gradlew.sh
+++ liferay-portal/sh/fn-gradlew.sh
@@ -1,13 +1,24 @@
+figlet -f digital -w 240 source fn-gradlew.sh || true
+
 function degradlew()
 {
-	mv settings.gradle settings.gradle.ORIGINAL || true
-	mv ../settings.gradle ../settings.gradle.ORIGINAL || true
+	:
+	#mv settings.gradle settings.gradle.ORIGINAL || true
+	#mv ../settings.gradle ../settings.gradle.ORIGINAL || true
 }
 
 function regradlew()
 {
-	mv settings.gradle.ORIGINAL settings.gradle || true
-	mv ../settings.gradle.ORIGINAL ../settings.gradle || true
+	:
+	#mv settings.gradle.ORIGINAL settings.gradle || true
+	#mv ../settings.gradle.ORIGINAL ../settings.gradle || true
+}
+
+function gradlew_baseline()
+{
+	degradlew
+	${LIFERAY_PORTAL_DIR}/gradlew baseline -a -q
+	regradlew
 }
 
 function gradlew_clean()
@@ -20,20 +31,21 @@ function gradlew_clean()
 function gradlew_deploy()
 {
 	degradlew
-	${LIFERAY_PORTAL_DIR}/gradlew deploy
+	figlet -f eftipiti -w 280 ${LIFERAY_PORTAL_DIR}/gradlew deploy -a -q || true
+	${LIFERAY_PORTAL_DIR}/gradlew deploy -a -q
 	regradlew
 }
 
 function gradlew_eclipse()
 {
 	degradlew
-	${LIFERAY_PORTAL_DIR}/gradlew eclipse -a
+	${LIFERAY_PORTAL_DIR}/gradlew eclipse -a -q
 	regradlew
 }
 
 function gradlew_format()
 {
 	degradlew
-	${LIFERAY_PORTAL_DIR}/gradlew formatSource -a
+	${LIFERAY_PORTAL_DIR}/gradlew formatSource -a -q
 	regradlew
 }
